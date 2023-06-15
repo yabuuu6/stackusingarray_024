cf13c2e090ebcc4b67bd9d652c11b29d7fd1407b
buyaramadhan <ytchochote@gmail.com>
2023-06-15 11:04:32 +0700

    commit10

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -59,7 +59,7 @@ int main() {
 
 	while (true) {
 		cout << endl;
-		cout << "\n***Stack Aldos***\n";
+		cout << "\n***Stack Menu***\n";
 		cout << "1. Push\n";
 		cout << "2. Pop\n";
 		cout << "3. Display\n";
@@ -70,8 +70,26 @@ int main() {
 		char ch = (input.empty() ? '0' : input[0]);
 		switch (ch) {
 		case '1': {
-
+			cout << "\nEnter an element: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
 		}
+		case '2':
+			if (s.empty()) {
+				cout << "\nStack is empty." << endl;
+				break;
+			}
+			s.pop();
+			break;
+		case '3':
+			s.display();
+			break;
+		case '4' :
+			return 0;
+		default:
+			cout << ""
 		}
 	}
 }
